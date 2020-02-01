22844c1d2721e1a32362ab4f50a7340d11af80a8
DD-a12 <hls112347@gmail.com>
2020-01-31 20:10:03 -0800

    worked a bit on ability file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true,
     format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-    has_many :rooms, dependent: :destroy 
-    has_many :booked_rooms , through: :booking, source: :room
-    
+    has_many :rooms, dependent: :nullify
+    has_many :courses, dependent: :nullify 
+    has_many :enrolled_courses, through: :enrollment, source: :course     
+
     private
 
     def full_name
