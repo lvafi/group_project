8ac1b664d531ccc381d6ab6821be34c917ec6991
Jiselle Liu <jiselle03@gmail.com>
2020-02-01 11:28:55 -0800

    Drop school model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_034819) do
+ActiveRecord::Schema.define(version: 2020_02_01_042124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "features", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -21,6 +27,11 @@ ActiveRecord::Schema.define(version: 2020_02_01_034819) do
     t.integer "capacity"
     t.float "price"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
