9cc35c5fc1abeebaa36b0144519da2f0679d316e
Jiselle Liu <jiselle03@gmail.com>
2020-02-01 22:47:24 -0800

    Add validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,15 +2,14 @@ class Review < ApplicationRecord
   # associations 
   belongs_to :course
   belongs_to :user
-
-  #has_many :reviewers, through: reviews, source: :user 
   
   # validations
-  validates :rating, {
+  validates :rating, 
+    presence: true,
     numericality:{
       greater_than_or_equal: 1,
-      less_than_equal_to: 5,
-    },
+      less_than_equal_to: 5
+    }
   end
-end
 
+end
