7fb3101a0123183d82b1a48080f786e9ad2feac0
DD-a12 <hls112347@gmail.com>
2020-02-01 17:12:34 -0800

    added booking.new on room#show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+    before_action :authenticate_user!, only: [:create, :update, :destroy]
     before_action :find_booking, only: [:update, :destroy]
     before_action :authenticate!, only: [:update, :destroy]
 
