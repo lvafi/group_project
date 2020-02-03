53f8129556938b521b83c04495e92b96e6fd6d23
DD-a12 <hls112347@gmail.com>
2020-02-03 03:59:12 -0800

    done some work

--- db/migrate/20200203110825_add_aasm_state_to_enrollments.rb
+++
@@ -1,5 +0,0 @@
-class AddAasmStateToEnrollments < ActiveRecord::Migration[6.0]
-  def change
-    add_column :enrollments, :aasm_state, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_02_03_095358) do
-=======
-ActiveRecord::Schema.define(version: 2020_02_03_110825) do
->>>>>>> f67e37e67c6c1f68591d507db2745e4d5bd6dbfb
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
